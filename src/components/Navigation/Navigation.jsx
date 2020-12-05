d0315ce12f8bc29a744f2b92511d493b3761fa44
Ruslan Buloychik <rusbuloichyk@gmail.com>
2020-12-05 20:31:29 +0300

    add Navigation component

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./style.css";
+
+export const Navigation = ({ children }) => {
+  const [isOpen, setToggleMenu] = useState(false);
+  const activeMenu = isOpen ? "open" : "";
+  const toggleMenuHandler = () => setToggleMenu(!isOpen);
+  return (
+    <>
+      <nav className={`navigation ${activeMenu}`}>
+        <div
+          className={`menu-icon ${activeMenu}`}
+          onClick={toggleMenuHandler}
+        ></div>
+        <div className="menu-wrap">{children}</div>
+      </nav>
+      <div
+        className={`overlay ${activeMenu}`}
+        onClick={toggleMenuHandler}
+      ></div>
+    </>
+  );
+};
